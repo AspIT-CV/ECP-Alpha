ab14b37dc22311b9bb3c4a8a792e991bf77f661d
Mads Mikkel Rasmussen <mara@aspit.dk>
2022-10-30 16:44:11 +0100

    Added Android app.

---
+++ AndroidApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AndroidApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
