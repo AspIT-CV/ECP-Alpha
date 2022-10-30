433027ddf76a070e72c41be818b1a2f15aab2187
Mads Mikkel Rasmussen <mara@aspit.dk>
2022-10-30 20:19:06 +0100

    Added a first version of events controller to the API.

---
+++ WebApi/Controllers/EventController.cs
@@ -0,0 +1,31 @@
+﻿using Entities;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApi.Controllers
+{
+    public class EventController : Controller
+    {
+        /// <summary>
+        /// Gets all events
+        /// </summary>
+        /// <returns>A collection of all events.</returns>
+        [HttpGet]
+        [Route("/GetAll")]
+        public async Task<ActionResult<IEnumerable<Event>>> GetAllEvents()
+        {
+            // TODO: replace this with the repo/UoW pattern:
+            try
+            {                
+                EcpContext context = new();
+                var allEvents = await context.Events.ToListAsync();
+                return Ok(allEvents);              
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, $"An error occured attempting to get all events.");
+            }                        
+        }
+    }
+}
