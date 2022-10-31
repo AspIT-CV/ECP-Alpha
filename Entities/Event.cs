de7a8fc59f5e15c746b78712fd9c3dad86caca7d
Mads Mikkel Rasmussen <mara@aspit.dk>
2022-10-31 11:45:26 +0100

    Removed more nullableness.

--- Entities/Event.cs
+++ Entities/Event.cs
@@ -8,14 +8,14 @@
         }
 
         public int EventId { get; set; }
-        public string Name { get; set; } = null!;
+        public string Name { get; set; }
         public string Description { get; set; }
-        public string Address { get; set; } = null!;
+        public string Address { get; set; }
         public DateTime? StartTime { get; set; }
         public DateTime? EndTime { get; set; }
         public int OrganizerIdFk { get; set; }
 
-        public virtual User Organizer { get; set; } = null!;
+        public virtual User Organizer { get; set; }
         public virtual ICollection<Assignment> Assignments { get; set; }
     }
 }
