75a4dd6b81bb51f8c56903e10f7601c18a670705
V1CT0R-AspIt <vict7147@edu.campusvejle.dk>
2022-11-02 13:36:14 +0100

    Made Volunteer Repo

---
+++ DataAccess/VolunteerRepository.cs
@@ -0,0 +1,16 @@
+﻿namespace DataAccess
+{
+	public class VolunteerRepository : GenericRepository<AssignmentVolunteer>
+	{
+		public VolunteerRepository(EcpContext context) : base(context)
+		{
+
+		}
+
+
+		public async Task<IEnumerable<Assignment>> GetAssignmentsFromVolunteerAndEvent(int volunteerID, int eventID)
+		{
+			return await dbSet.Where(s => s.UserId == volunteerID && s.Assignment.EventIdFk == eventID).Select(s => s.Assignment).ToListAsync();
+		}
+	}
+}
