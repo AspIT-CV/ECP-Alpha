d47231e5a8042dbe0ffafc10c294853a734fcbe3
nikl3009 <nikl3009@CV-BB-5483>
2022-11-03 12:55:10 +0100

    Added some of usercontroller cant and wont add more cuease the datebase sucks

---
+++ DataAccess/UserRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace DataAccess
+{
+    public class UserRepository : GenericRepository<User>
+    {
+        public UserRepository(EcpContext context) : base(context)
+        {
+        }
+
+        public async Task<IEnumerable<Event>> GetEventsSeekingVolunteers()
+        {
+            var y = await context.Events.Where(s => s.Assignments.Any(s => s.VolunteersRequested - context.AssignmentVolunteers.Where(t => t.AssignmentId == s.AssignmentId).Select(o => o.AssignmentId).Count() > 0)).ToListAsync();
+            return y;
+        }
+    }
+}
---
+++ WebApi/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using DataAccess;
+using Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        UserRepository userRepository = new(new());
+
+        [HttpGet("ESeekingV")]
+        public async Task<ActionResult<IEnumerable<Event>>> GetEventsSeekingVolounteers()
+        {
+            try
+            {
+                return Ok(await userRepository.GetEventsSeekingVolunteers());
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, $"An error occured attempting to get events seeking volunteers\n{e}");
+            }
+        } 
+    }
+}
