77ba245f34f7cd961a35ef5b1fbe2a6824a22b0d
nikl3009 <nikl3009@CV-BB-5483>
2022-11-03 11:04:05 +0100

    Added AssignmentController

---
+++ DataAccess/AssignmentRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace DataAccess
+{
+    public class AssignmentRepository : GenericRepository<Assignment>
+    {
+        public AssignmentRepository(EcpContext context) : base(context)
+        {
+        }
+
+        public async Task<IEnumerable<Assignment>> GetAssignmentsByEvent(int eventId)
+        {
+            return await dbSet.Where(s => s.EventIdFk == eventId).ToListAsync();
+        }
+    }
+}
---
+++ WebApi/Controllers/AssignmentController.cs
@@ -0,0 +1,72 @@
+﻿using DataAccess;
+using Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AssignmentController : ControllerBase
+    {
+        AssignmentRepository assignmentRepository = new(new());
+
+        [HttpGet("{eventId}")]
+        public async Task<ActionResult<IEnumerable<Assignment>>> GetAssignmentsByEvent(int eventId)
+        {
+            try
+            {
+                return Ok(await assignmentRepository.GetAssignmentsByEvent(eventId));
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, $"An error occured attempting to get assignments by event");
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddAssignment(Assignment assignment)
+        {
+            try
+            {
+                await assignmentRepository.InsertAsync(assignment);
+                await assignmentRepository.SaveAsync();
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, $"An error occured attempting to add assignment");
+            }
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateAssignment(Assignment assignment)
+        {
+            try
+            {
+                await assignmentRepository.UpdateAsync(assignment);
+                await assignmentRepository.SaveAsync();
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, $"An error occured attempting to update assignment");
+            }
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteAssignment(int assignmentId)
+        {
+            try
+            {
+                await assignmentRepository.DeleteAsync(assignmentId);
+                await assignmentRepository.SaveAsync();
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, $"An error occured attempting to delete assignment");
+            }
+        }
+    }
+}
